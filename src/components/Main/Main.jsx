7065021673c21a575abfbc9f43f3e51fbd7658c1
Nícolas <nicolasbretschneider1@gmail.com>
2025-11-14 09:20:17 -0300

    feat: adiciona hero section estilizada

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,14 @@
+import styles from '../../styles/main.module.css'
+
 export default function Main() {
     return (
-        <h1>Main</h1>
+        <main>
+            <section className={styles.heroSection}>
+                <div className={styles.titleContainer}>
+                    <h1>CUSTOM CODE</h1>
+                    <span>DANDO FORMA A IDEIAS</span>
+                </div>
+            </section>
+        </main>
     )
 }

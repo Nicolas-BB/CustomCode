e293843dc481f42d660bf098b241c40f9229fe47
Nícolas <nicolasbretschneider1@gmail.com>
2025-11-14 15:13:12 -0300

    refactor:  altera nome do export do button

--- src/components/utils/Buttons.jsx
+++ src/components/utils/Buttons.jsx
@@ -1,6 +1,6 @@
 import styles from '../../styles/button.module.css'
 
-export default function Button(props) {
+export function PrimaryButton(props) {
     return (
         <a href={props.href} className={styles.button}>{props.children}</a>
     )

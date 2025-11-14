9d8d061d35f319aecabc8d693f2276e3e31a5ee3
Nícolas <nicolasbretschneider1@gmail.com>
2025-11-14 09:36:22 -0300

    feat: adiciona logo centralizada no header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import Logo from '../../images/Logo.png'
 import styles from '../../styles/header.module.css'
 
 export default function Header() {
     return (
         <header className={styles.header}>
+            <img src={Logo} alt="Logo da Custom Code" className={styles.logo} />
             <nav>
                 <ul className={styles.ul}>
                     <li><a href="#">Início</a></li>
